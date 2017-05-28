b2524d2af4466cbc4e3293c2a77445571c6166bd
RsuhanB <Rushanben@hotmail.com>
2017-05-28 02:21:45 -0400

    Working Commit

---
+++ SwiftyCats/Photo.swift
@@ -0,0 +1,13 @@
+//
+//  Photo.swift
+//  SwiftyCats
+//
+//  Created by Rushan on 2017-05-28.
+//  Copyright © 2017 RushanBenazir. All rights reserved.
+//
+
+import UIKit
+
+class Photo: NSObject {
+
+}
---
+++ SwiftyCats/PhotoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewCell.swift
+//  SwiftyCats
+//
+//  Created by Rushan on 2017-05-28.
+//  Copyright © 2017 RushanBenazir. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+}
